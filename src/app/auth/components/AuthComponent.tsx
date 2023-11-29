aa69755b3dca4b506085d056f331de39c36e05bd
Mr.Void <72743789+kirtanlab@users.noreply.github.com>
2023-11-29 21:28:32 +0530

    Update AuthComponent.tsx

--- src/app/auth/components/AuthComponent.tsx
+++ src/app/auth/components/AuthComponent.tsx
@@ -39,7 +39,7 @@ export default function AuthComponent() {
       supabase.auth.signInWithOAuth({
         provider: "google",
         options: {
-          redirectTo: location.origin + "/auth/callback",
+          redirectTo: "https://singular-creponne-6ac827.netlify.app/auth/auth/callback",
         },
       });
     };
