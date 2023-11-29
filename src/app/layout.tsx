532a968bc1b642197f2ee9cdf924eff77248cb2d
Mr.Void <72743789+kirtanlab@users.noreply.github.com>
2023-11-29 21:56:33 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,11 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<html lang="en">
+		<html lang="en" >
+			<meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+      ></meta>
 			<body className={inter.className}>
 				{children} <Toaster />
 			</body>
